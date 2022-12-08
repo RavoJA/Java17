19573e7258cac5710b95f30ac6495c0eb3a7e697
Jean Aimé RAVOMANANA <jeanaime.ravomanana@gmail.com>
2022-12-08 14:52:59 +0300

    initial commit

---
+++ src/main/java/jar/dev/java17overview/Java17overviewApplication.java
@@ -0,0 +1,13 @@
+package jar.dev.java17overview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java17overviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java17overviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jar/dev/java17overview/Java17overviewApplicationTests.java
@@ -0,0 +1,13 @@
+package jar.dev.java17overview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java17overviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
